e3e7372990844c755f40d246036b7b3c268e890f
abhijeet <104857527+imabhijeet@users.noreply.github.com>
2024-02-18 11:17:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 // XML http request
-const requestUrl = 'https://api.github.com/users/imabhijeet';
-const xhr = new XMLHttpRequest();
-xhr.open('GET', requestUrl);
 
-// whenever the state changes, function will be called
-xhr.onreadystatechange = function () {
-    console.log(xhr.readyState);
-    if (xhr.readyState === 4) {
+function fetchData() {
+    const requestUrl = 'https://api.github.com/users/imabhijeet';
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', requestUrl);
 
-        //convert the string content to json type
-        const data = JSON.parse(this.responseText); 
+    // whenever the state changes, function will be called
+    xhr.onreadystatechange = function () {
+        console.log(xhr.readyState);
+        if (xhr.readyState === 4) {
 
-        //use ID to update the element because it targets the unique elements
-        document.getElementById('userName').innerHTML = data.login;
-        document.getElementById('userFollowers').innerHTML =`Followers: ${data.followers}`;
-        document.getElementById('userFollowing').innerHTML = `Following: ${data.following}`; 
-        document.getElementById('publicRepos').innerHTML = `Public Repos: ${data.public_repos}`;
-        
-        //profile pic
-        const profileImageElement = document.getElementById('user-image');
-        profileImageElement.src = data.avatar_url;
+            //convert the string content to json type
+            const data = JSON.parse(this.responseText);
+            // console.log(typeof data); //string
+            console.log(data);
+
+            //use ID to update the element because it targets the unique elements
+            document.getElementById('userName').innerHTML = data.login;
+            document.getElementById('userFollowers').innerHTML = `Followers: ${data.followers}`;
+            document.getElementById('userFollowing').innerHTML = `Following: ${data.following}`;
+            document.getElementById('publicRepos').innerHTML = `Public Repos: ${data.public_repos}`;
+
+            //profile pic
+            const profileImageElement = document.getElementById('user-image');
+            profileImageElement.src = data.avatar_url;
+        }
     }
+    xhr.send();
 }
-xhr.send();
+
+// function to update the data frequently
+function updateData() {
+    // fetch the data for first time
+    fetchData()
+    // fetch data every 5 minutes
+    setInterval(fetchData, 3600000);
+}
+
+updateData();
